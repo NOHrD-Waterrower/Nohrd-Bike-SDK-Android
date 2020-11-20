53bcb76bf1527b403dfd131eede9020e4baf4423
Niek Haarman <niek@305.nl>
2020-11-20 11:05:23 +0100

    Set up devices screen

---
+++ sdk-ble/sample/src/main/java/com/nohrd/bike/sdk/ble/sample/ui/devices/Device.kt
@@ -0,0 +1,6 @@
+package com.nohrd.bike.sdk.ble.sample.ui.devices
+
+data class Device(
+    val address: String,
+    val name: String
+)
--- sdk-ble/sample/src/main/java/com/nohrd/bike/sdk/ble/sample/ui/devices/DevicesActivity.kt
+++ sdk-ble/sample/src/main/java/com/nohrd/bike/sdk/ble/sample/ui/devices/DevicesActivity.kt
@@ -1,5 +1,19 @@
 package com.nohrd.bike.sdk.ble.sample.ui.devices
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.setContent
 
-class DevicesActivity : AppCompatActivity()
+class DevicesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            DevicesView(
+                devices = emptyList(),
+                onDeviceClick = {}
+            )
+        }
+    }
+}
