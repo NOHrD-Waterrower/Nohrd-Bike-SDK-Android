1a8db599c5aabef37ac5a936a4d1cc77c65e67d6
Niek Haarman <niek@305.nl>
2020-11-24 14:59:57 +0100

    Read bike data

--- sdk/src/main/kotlin/com/nohrd/bike/sdk/Calibration.kt
+++ sdk/src/main/kotlin/com/nohrd/bike/sdk/Calibration.kt
@@ -1,6 +1,6 @@
 package com.nohrd.bike.sdk
 
 public data class Calibration(
-    val lowValue: ResistanceMeasurement,
-    val highValue: ResistanceMeasurement,
+    val lowValue: ResistanceMeasurement, // Min is ~450
+    val highValue: ResistanceMeasurement, // Max is ~4000
 )
